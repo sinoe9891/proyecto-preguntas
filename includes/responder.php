d94fe84ed1d7476f916457702ab94c6a7c490d5e
sinoe9891 <sinoeproducciones@gmail.com>
2021-11-20 22:39:59 -0600

    agregar respuesta

---
+++ includes/responder.php
@@ -0,0 +1,19 @@
+<?php
+	$boton = $_POST['nueva_pregunta']; // Información de Botón
+	$nueva_pre = $_POST['new_question']; //Información del campo
+
+	// if (isset($boton)) {
+	// 	//Conexión DB
+	// 	include 'conexion.php';
+	// 	//Declarando una variable con el Query
+	// 	$sqlquery = "INSERT INTO preguntas (preguntas, estado) VALUES ('$nueva_pre', 1)";
+	// 	// Enviar datos por medio de la conexión y enviar query
+	// 	// mysqli_query($conn, $sqlquery);
+	// 	if ($conn->query($sqlquery)===true) {
+	// 		header('Location: ../agregar.php?envio=1');
+	// 	}else{
+	// 		header('Location: ../agregar.php?envio=2');
+	// 		die($conn->error);
+	// 	}
+	// }
+?>
--- responder.php
+++ responder.php
@@ -1,6 +1,9 @@
-
+<?php
+include 'includes/conexion.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,6 +11,7 @@
 	<link rel="stylesheet" href="css/style.css">
 	<title>Document</title>
 </head>
+
 <body>
 	<div class="main">
 		<h1>Proyecto Preguntas</h1>
@@ -17,26 +21,84 @@
 	</a>
 	<div class="container">
 		<h2>Listado de Preguntas</h2>
+
 		<table>
 			<tr>
 				<th>No.</th>
 				<th>Pregunta</th>
-				<th>Estado</th>
 				<th>Responder</th>
+				<th>Estado</th>
+				<th>Gestionar</th>
 			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
-			<tr>
-				<td></td>
-			</tr>
+			<?php
+			if ($conn) {
+				$resp = $_GET['id'];
+				$preguntas_query = "SELECT * FROM preguntas";
+				$respoder_resp = "SELECT * FROM preguntas WHERE id = '$resp'";
+				$result = $conn->query($preguntas_query);
+				$resput = $conn->query($respoder_resp);
+				print_r($resp);
+				$numero = 0;
+				while ($question = $result->fetch_assoc()) {
+					$numero++;
+					$preguntas = $question['preguntas'];
+					$respuesta = $question['respuesta'];
+					$estado = $question['estado'];
+					$id = $question['id'];
+					?>
+					<tr>
+						<td>
+							<p><?php echo $numero; ?></p>
+						</td>
+						<td>
+							<p><?php echo $preguntas; ?></p>
+						</td>
+						<td>
+							<p><?php echo $respuesta; ?></p>
+						</td>
+						<td>
+							<p><?php echo $estado = 1 ? "Respondida" : "No Respondida"; ?></p>
+						</td>
+						<td>
+							<a href="responder.php?id=<?php echo $id ?>">
+								<p>Responder</p>
+							</a>
+						</td>
+					</tr>
+			<?php
+				}
+				$conn->close();
+			} else {
+				echo 'Conexión Fallida';
+			};
+
+			while ($answer = $resput->fetch_assoc()) {
+				$pregunt = $answer['preguntas'];
+				print_r($pregunt);
+				?>
+
+			<div style="<?php echo $ver; ?>">
+				<h4>Responder Pregunta</h4>
+				<p>¿<?php echo $pregunt; ?></p>
+				<form action="">
+					<div>
+						<textarea name="contenido_respuesta" id="" cols="30" rows="3"></textarea>
+					</div>
+					<div>
+						<input type="submit" name="respondio" value="Guardar Respuesta">
+					</div>
+				</form>
+			</div>
+				<?php
+			}s
+			$ver = "display:none";
+			if ($resp) {
+				$ver = "display:initial";
+			};
+			?>
+			
 		</table>
 	</div>
 </body>
+
 </html>
